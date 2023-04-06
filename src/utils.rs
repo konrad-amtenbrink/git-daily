4e1c160bbc9574b4a0957e574ed16aa36d347d36
Konrad Amtenbrink <konrad02@gmx.de>
2023-04-06 12:01:13 +0200

    feat: add duration instead of date

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,44 @@
 use crate::git::CommitData;
+use colored::Colorize;
 
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, Duration, NaiveDateTime, Utc};
 
 pub(crate) fn pretty_print_commits(commits: Vec<CommitData>) {
     for commit in commits {
-        print!(
-            "({})   {} - {}\n",
-            commit.commit_date, commit.commit_message, commit.commit_author,
+        println!(
+            "{0: <20} {1: <50} {2: <20}",
+            commit.commit_author.bright_blue(),
+            commit.commit_message.yellow(),
+            commit.commit_date.green(),
         );
     }
 }
 
 pub(crate) fn convert_time(timestamp: i64) -> String {
+    // FIXME remove deprecated function
     let naive = NaiveDateTime::from_timestamp(timestamp, 0);
     let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
-    let newdate = datetime.format("%Y-%m-%d %H:%M:%S");
 
-    return newdate.to_string();
+    let end_time: DateTime<Utc> = Utc::now();
+    let diff = end_time - datetime;
+
+    return get_formatted_duration(diff);
+}
+
+fn get_formatted_duration(diff: Duration) -> String {
+    let mut duration: i64 = diff.num_seconds();
+    let mut unit = "seconds";
+    if duration > 60 {
+        duration = diff.num_minutes();
+        unit = "minutes";
+    }
+    if duration > 60 {
+        duration = diff.num_hours();
+        unit = "hours";
+    }
+    if duration > 24 {
+        duration = diff.num_days();
+        unit = "days";
+    }
+    return format!("{} {} ago", duration, unit);
 }
